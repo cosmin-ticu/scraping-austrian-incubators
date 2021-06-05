28f21e7611d28fed035de23e499d33e193210d9e
Cosmin Catalin Ticu <cosmin.cata99@gmail.com>
2021-06-05 22:25:59 +0200

    added language detection on titles as well

--- pre-processing_scripts/adding_language_to_base_df.R
+++ pre-processing_scripts/adding_language_to_base_df.R
@@ -49,3 +49,31 @@ write.csv(all_incubators_raw_content_languages ,
           file = "data/incubators_raw_content_languages.csv",
           fileEncoding = "utf-8",
           row.names = F)
+
+# Post analysis / extra AWS analysis --------------------------------------
+
+# Language detection with AWS for article titles
+# Function that detects main language of article through AWS comprehend according to first 400 characters
+get_language_scores_titles <- function(){
+  ret_df <- rbindlist(lapply(all_incubators_raw_content[[3]], 
+                             function(content){
+                               
+                               Sys.sleep(2)
+                               
+                               t_list <- detect_language(content)[1,2:3]
+                               
+                               return(data.frame(t_list))
+                             }))
+  return(ret_df)
+}
+
+language_scores <- get_language_scores_titles()
+
+all_incubators_raw_content_languages_titles <- cbind(all_incubators_raw_content, language_scores)
+
+# Write out the data 
+# Write base file containing full content
+write.csv(all_incubators_raw_content_languages_titles , 
+          file = "data/incubators_raw_content_languages_titles(ad-hoc).csv",
+          fileEncoding = "utf-8",
+          row.names = F)
